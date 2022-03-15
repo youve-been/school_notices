a49e8e27a764b40cf28b3944876936fe0573b021
youve-been <79850683+youve-been@users.noreply.github.com>
2022-03-16 01:57:22 +0900

    second commit
    only updated notices will be announced by Slack / notice links will be announced with the titles of the notices / extra database text file(written in Korean) for checking updates added

--- parser.py
+++ parser.py
@@ -1,6 +1,12 @@
+#this file is for testing web crawling before at post_slack.py
+
 import requests
 from bs4 import BeautifulSoup as bs
+import time
 
+fp = open('titles_db.txt', 'w')
+
+#while True :
 page = requests.get("https://scc.sogang.ac.kr/front/cmsboardlist.do?siteId=cs&bbsConfigFK=1905")
 soup = bs(page.text, "html.parser")
 
@@ -11,6 +17,25 @@ while True :
         break
 
 elements = soup.select('div.list_box ul > li > div > a')
+posting = []
+for i in elements :
+    posting.append('*'+i.get_text()+'*/n'+'https://cs.sogang.ac.kr'+i.get('href'))
 
-for index, element in enumerate(elements, 1):
-    print("{} 번째 게시글의 제목 : {}".format(index, element.string))
+fp.write('\n'.join(posting))
+    
+#testing prints
+#for idx, element in enumerate(title, 1):
+#    print("{} 번째 게시글의 제목 : {}".format(idx, element))
+#    print("링크 : {}".format(link[idx]))
+#print()
+
+#find updates - for post_slack.py   
+#if last_title != title :
+#    for t in title :
+#        if t not in last_title :
+#            print('sth changed')
+#last_title = title
+
+#time.sleep(300)
+
+fp.close()
--- post_slack.py
+++ post_slack.py
@@ -1,5 +1,7 @@
+from ntpath import join
 import requests
 
+#function that sends Slack messages
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
@@ -7,8 +9,15 @@ def post_message(token, channel, text):
     )
     print(response)
 
-myToken = "xoxb-3249471790705-3234025928821-grduLvQfqcluijTzllxJi4AH"
+#channel and token info
+posting_channel = '#컴공_주요공지'
+myToken = "[token]"
  
+
+rfp = open('titles_db.txt', 'r')
+
+
+#reference : parser.py 
 from bs4 import BeautifulSoup as bs
 
 page = requests.get("https://scc.sogang.ac.kr/front/cmsboardlist.do?siteId=cs&bbsConfigFK=1905")
@@ -21,7 +30,19 @@ while True :
         break
 
 elements = soup.select('div.list_box ul > li > div > a')
+posting = []
+for i in elements :
+    posting.append('*'+i.get_text()+'*/n'+'https://cs.sogang.ac.kr'+i.get('href'))
+    
+last_post = list(map(lambda s : s.rstrip(), rfp.readlines()))
+    
+if last_post != posting :
+    for t in posting :
+        if t not in last_post :
+            post_message(myToken, posting_channel, t)
 
-for index, element in enumerate(elements, 1):
-    post_message(myToken,"#컴공_주요공지", element)
+rfp.close()
 
+wfp = open('titles_db.txt', 'w')
+wfp.write('\n'.join(posting))
+wfp.close()
